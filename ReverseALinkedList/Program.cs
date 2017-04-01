a769c00d1617eb1ed2940430b9ee5a781b989ff0
John <johnthomasp@gmail.com>
2017-04-01 13:05:01 -0700

    Intial Check in

---
+++ ReverseALinkedList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseALinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    
+
+
+    class LinkedList
+    {
+        Node head;
+
+       public class Node
+       {
+            public int data;
+            public Node next;
+
+            Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+
+        public Node ReverseList(Node node)
+        {
+            Node next = null;
+            Node current = node;
+            Node prev = null;
+
+
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+
+            head = prev;
+
+            return node;
+        }
+    }
+}
