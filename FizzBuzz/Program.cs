3d2eb8a60c9446596206f2e85e6734bbd487ab72
John Thomas <johnthomasp@gmail.com>
2017-10-24 16:40:08 -0700

    Fizbuzz Code

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+
+    class Program
+    {
+        public static void FizzBuzz(int x)
+        {
+            for (int i = 1; i <= x; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0 )
+                    Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int x = int.Parse(Console.ReadLine());
+            FizzBuzz(x);
+            Console.ReadLine();
+        }
+    }
+}
