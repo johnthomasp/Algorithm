d8b61ef180708ec30a7c8a518fe09e5d9cecb9af
John Thomas <johnthomasp@gmail.com>
2018-05-31 13:02:01 -0700

    Stock Total Profit

--- BuySellStock/Program.cs
+++ BuySellStock/Program.cs
@@ -30,12 +30,27 @@ namespace BuySellStock
             return mxp;
         }
 
+        private static int CalculateTotalProfit(int[] stockPrices)
+        {
+            int totalProfit = 0;
+
+            for (int i = 1; i < stockPrices.Length; i++)
+            {
+                int profit = stockPrices[i] - stockPrices[i - 1];
+                if (profit > 0)
+                    totalProfit += profit;
+            }
+            return totalProfit;
+        }
+
         static void Main(string[] args)
         {
             int[] stockPrices = { 7, 1, 5, 3, 6, 4 };
             //int[] stockPrices = { 7, 6, 4, 3, 1 };
             int maxProfit = CalculateMaxProfit(stockPrices);
+            int totalProfit = CalculateTotalProfit(stockPrices);
             Console.WriteLine("The maximum profit for the stock is {0}", maxProfit);
+            Console.WriteLine("The total profit for the stock is {0}", totalProfit);
             Console.ReadLine();
         }
     }
