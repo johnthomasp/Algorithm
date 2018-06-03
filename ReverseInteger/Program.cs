53666de7df1b2cbcc0d1507c87c13fdbc942326d
John Thomas <johnthomasp@gmail.com>
2018-06-03 10:33:26 -0700

    Reverse Integer

---
+++ ReverseInteger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ReverseInteger
+{
+    class program
+    {
+        static int ReverseNumber(int num)
+        {
+            int rev_num = 0;
+            while (num > 0)
+            {
+                rev_num = rev_num * 10 + num % 10;
+                num = num / 10;
+            }
+
+            return rev_num;
+        }
+
+        static void Main(string[] args)
+        {
+            int num = 1234;
+            int result = ReverseNumber(num);
+
+            Console.WriteLine("The reverse of {0} is {1}", num, result);
+            Console.ReadLine();
+
+
+        }
+    }
+}
