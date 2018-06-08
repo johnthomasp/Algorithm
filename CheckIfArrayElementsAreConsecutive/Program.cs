30324a5355f14846f160496ec3e0a2cde87b4587
John Thomas <johnthomasp@gmail.com>
2018-06-08 12:24:17 -0700

    Code Checkin

---
+++ CheckIfArrayElementsAreConsecutive/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIfArrayElementsAreConsecutive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 7, 1, 5, 3, 6, 4 };
+            int result = CheckElementsAreConsecutive(arr);
+            Console.WriteLine("The total of array is {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
