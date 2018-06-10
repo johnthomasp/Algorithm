e67dd20110596921aff6b262a0aedaa02346b23d
John Thomas <johnthomasp@gmail.com>
2018-06-09 22:07:17 -0700

    Checkin Minimum Subarrar Problem

--- MinimumSizeSubarraySum/Program.cs
+++ MinimumSizeSubarraySum/Program.cs
@@ -16,8 +16,35 @@ namespace MinimumSizeSubarraySum
 {
     class Program
     {
+        public static int CalculateMinimumSizeSubArray(int[] arr, int value)
+        {
+            int sum = 0;
+            int start = 0;
+            int min = arr.Length + 1;
+            for (int end = 0; end < arr.Length; end++)
+            {
+                sum += arr[end];
+                while (sum >= value)
+                {
+                    min = Math.Min(min, end - start + 1);
+                    sum -= arr[start];
+                    start++;
+                }
+            }
+
+            return (min == arr.Length + 1) ? 0: min;
+        }
+
         static void Main(string[] args)
         {
+            //           int[] arr = {2,3,1,2,4,3};
+            //           int[] arr = {  3, 1, 2, };
+            int[] arr = { 3, 1, 2, };
+
+            int value = 7;
+            int result = CalculateMinimumSizeSubArray(arr, value);
+            Console.WriteLine("The minimum values needed to get a sum of {0} is {1}", value, result);
+            Console.ReadLine();
         }
     }
 }
